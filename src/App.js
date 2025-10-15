85a968c5d082324ea56f3d0484c102ad701cc97b
preranabhamare <preranabhamare04@gmail.com>
2025-10-15 09:15:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit = {handleSubmit}>
+      <input type ="number" placeholder="Enter First Number" value={num1} onChange={(e) => setNum1(e.target.value)}></input>
+      <input type ="number" placeholder="Enter Second Number" value={num2} onChange={(e) => setNum2(e.target.value)} ></input>
+
+      <button type='submit'>Add</button>
+      <h2>Result : {result}</h2>
+      </form></>
   );
 }
 
